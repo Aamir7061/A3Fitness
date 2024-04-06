098f434fddb405caf8c6b34bc44a6d0280e6d822
Aamir Azim Ansari <53116598+Aamir7061@users.noreply.github.com>
2024-04-06 13:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -479,6 +479,3 @@ def finished():
         return render_template('finished.html',username=session['username'], performance_data=total_exer,total_duration='Total Duration : {}'.format(a))
     else:
         return render_template('finished.html',username='Guest', performance_data=total_exer,total_duration='Total Duration : {}'.format(a))
-
-if __name__ == "__main__":
-    app.run(debug=True)
